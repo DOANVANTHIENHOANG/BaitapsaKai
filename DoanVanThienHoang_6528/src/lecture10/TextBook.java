5f82f2ded98b51c2018959ce1dbdcb1a052db666
DOANVANTHIENHOANG <doanvthienhoang910@gmail.com>
2025-02-22 03:33:48 +0700

    thien hoang

---
+++ DoanVanThienHoang_6528/src/lecture10/TextBook.java
@@ -0,0 +1,25 @@
+
+package lecture10;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class TextBook extends Book {
+    private String status;
+    public TextBook(String bookId,String publisher,Date entryDate, double unitPrice,double quantity,String status){
+        super(bookId,publisher,entryDate,unitPrice,quantity);
+        this.status = status;
+    }
+    @Override
+    public void addBook(){
+        super.addBook();
+        System.out.println("Enter status (TextBook): ");
+        Scanner scanner = new Scanner(System.in);
+        this.status = scanner.nextLine();
+    }
+    @Override
+    public void displayBook(){
+        super.displayBook();
+        System.out.println("Status: " + status);
+    }
+}
